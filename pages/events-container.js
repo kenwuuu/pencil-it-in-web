997325b7114e40b6d7630d69a6437f8ef73f18c7
kenwu <kenqiwu@gmail.com>
2025-04-24 10:56:34 -0700

    feat: add event filter buttons

--- pages/events-container.js
+++ pages/events-container.js
@@ -10,7 +10,13 @@ class EventsContainer extends HTMLElement {
           hx-trigger="load"
           hx-target=".events-agenda"
         >
-          <h1 x-text="capitalize(page)"></h1>
+          <div id="page-header" class="flex">
+            <h1 x-text="capitalize(page)" class="flex-1"></h1>
+            <div class="join">
+              <button class="btn join-item">All Events</button>
+              <button class="btn join-item">My Events</button>
+            </div>
+          </div>
           <div class="events-agenda not-prose"></div>
         </div>
         <action-menu></action-menu>
