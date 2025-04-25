4ef079a06f419827bd6e951af575c5853cd97ae5
kenwu <kenqiwu@gmail.com>
2025-04-25 07:39:07 -0700

    feat: add login

---
+++ services/login.js
@@ -0,0 +1,18 @@
+import {supabase} from './supabase-client/supabase-client.js';
+
+/**
+ * Logs in with email & password.
+ * @param {string} email
+ * @param {string} password
+ * @returns {Promise<{ user: object | null, session: object | null, error: object | null }>}
+ */
+export async function loginWithEmailPassword(email, password) {
+    const {
+        data: {user, session},
+        error,
+    } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+    });
+    return {user, session, error};
+}
