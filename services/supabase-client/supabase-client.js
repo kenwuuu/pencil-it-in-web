7be51874c7a150947068aced2ff68e72b4c3b175
kenwu <kenqiwu@gmail.com>
2025-05-01 06:05:22 -0700

    feat: add boilerplate for creating Supabase with Auth

--- services/supabase-client/supabase-client.js
+++ services/supabase-client/supabase-client.js
@@ -4,8 +4,16 @@ export const SUPABASE_URL = 'https://mpounklnfrcfpkefidfn.supabase.co';
 export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1wb3Vua2xuZnJjZnBrZWZpZGZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIxODE0OTcsImV4cCI6MjA1Nzc1NzQ5N30.wZlH6_dd0WtEVC-BtMXEzcTUgSAIlegqSPnr3dyvjyA';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabaseWithAuth = null;
 
-export function getSupabaseClientWithAuth(accessToken) {
+function getAccessToken() {
+    const accessToken = "todo";
+    return accessToken;
+}
+
+export function getSupabaseClientWithAuth() {
+    const accessToken = getAccessToken();
+
     return createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
         global: {
             headers: {
