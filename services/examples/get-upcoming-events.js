5995236abf6ebff4f3b5a70c177b999da00c8bee
kenwu <kenqiwu@gmail.com>
2025-05-01 07:44:55 -0700

    fix: remove unnecessary auth
    client contains necessary info to authenticate with API after we sign in

--- services/examples/get-upcoming-events.js
+++ services/examples/get-upcoming-events.js
@@ -1,4 +1,4 @@
-import {getSupabaseClientWithAuth, supabase} from "../supabase-client/supabase-client.js";
+import {supabase} from "../supabase-client/supabase-client.js";
 
 const {data: signInData, error: signInError} = await supabase.auth.signInWithPassword({
     email: 'kenqiwu@gmail.com',
@@ -9,9 +9,7 @@ if (signInError) {
     console.error('Sign-in error:', signInError);
 }
 
-const supabaseClientWithAuth = getSupabaseClientWithAuth(signInData.session.access_token);
-
-const {data: rpcData, error: rpcError} = await supabaseClientWithAuth.rpc('get_upcoming_events', {
+const {data: rpcData, error: rpcError} = await supabase.rpc('get_upcoming_events', {
     querying_user_id: 'f18d6f00-b861-45bd-bad9-2d3c1b772323'
 });
 
