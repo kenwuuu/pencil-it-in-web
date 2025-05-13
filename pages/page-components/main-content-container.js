e18186b0f352c653125dd843a420b17589c91dec
kenwu <kenqiwu@gmail.com>
2025-05-13 09:50:44 -0700

    chore: autoformatting

--- pages/page-components/main-content-container.js
+++ pages/page-components/main-content-container.js
@@ -4,14 +4,14 @@ import '../profile-container.js';
 import '../settings-container.js';
 
 class MainContentContainer extends HTMLElement {
-  connectedCallback() {
-    this.innerHTML = `
+    connectedCallback() {
+        this.innerHTML = `
       <events-container x-show="page === 'events'"></events-container>
       <friends-container x-show="page === 'friends'"></friends-container>
       <profile-container x-show="page === 'profile'"></profile-container>
       <settings-container x-show="page === 'settings'"></settings-container>
     `;
-  }
+    }
 }
 
 customElements.define('main-content-container', MainContentContainer);
