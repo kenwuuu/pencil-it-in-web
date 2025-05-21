ea8f9dd06f7e6b2c9962ddc4bb3423c2778827a5
kenwu <kenqiwu@gmail.com>
2025-05-20 20:41:23 -0700

    feat: add friend search boilerplate page

--- src/friends/friends-container.js
+++ src/friends/friends-container.js
@@ -1,22 +1,24 @@
 import './friends-action-menu.js';
+import './friend-search-component.js';
 
 class FriendsContainer extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-      <div class="flex">
+      <main class="flex" x-data=" { is_searching_for_friends: false }">
         <div
           class="page-container"
           hx-get="src/friends/mock_data/friends.html"
           hx-trigger="load"
           hx-target=".friends-list"
         >
-          <div class="prose mb-6">
+          <header class="prose mb-6">
             <h1 x-text="capitalize(page)"></h1>
-          </div>
-          <div class="friends-list not-prose"></div>
+          </header>
+          <section class="friends-list not-prose" x-show="!is_searching_for_friends"></section>
+          <friend-search-component x-show="is_searching_for_friends"></friend-search-component>
         </div>
         <friends-action-menu src="src/mock_data/action_menu/friends.html"></friends-action-menu>
-      </div>
+      </main>
     `;
     }
 }
