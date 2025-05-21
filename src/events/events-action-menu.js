a56e6f8db65cc51c9dcd8ce3ffbe3be56ce177f0
kenwu <kenqiwu@gmail.com>
2025-05-20 18:12:34 -0700

    fix: formatting

--- src/events/events-action-menu.js
+++ src/events/events-action-menu.js
@@ -3,9 +3,7 @@ class EventsActionMenu extends HTMLElement {
         this.innerHTML = `
     <ul class="menu menu-xl w-80 bg-base-200 lg:menu-vertical rounded-box"
     x-data="{ current_page: window.location.pathname }">
-        <li 
-            x-on:click=" is_creating_new_event = true ">
-            
+        <li x-on:click=" is_creating_new_event = true ">
             <a class="sidebar-menu-item">
                 <span class="iconify" data-icon="mdi-plus"></span>
                 Create Event
