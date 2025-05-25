33b4b266d8138e4bafd06568dc2f444c255bee8f
kenwu <kenqiwu@gmail.com>
2025-05-25 15:20:34 -0700

    fix: vite build works now

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,18 @@
 import {defineConfig} from 'vite';
+import {resolve} from 'path';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        events: resolve(__dirname, 'events.html'), // Assuming app.html is in the root
+        login: resolve(__dirname, 'src/auth/login.html'), // <--- Add this line for login.html
+        // Add any other top-level HTML files here
+      },
+    },
+  },
+
 });
