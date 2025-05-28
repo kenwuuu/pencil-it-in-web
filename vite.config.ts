fd2f15207cb1c348e24273ada85994a10f391a97
kenwu <kenqiwu@gmail.com>
2025-05-27 23:46:19 -0700

    fix: little testy test

--- vite.config.ts
+++ vite.config.ts
@@ -10,12 +10,13 @@ export default defineConfig({
         index: resolve(__dirname, 'index.html'),
         events: resolve(__dirname, 'events.html'), // Assuming app.html is in the root
         login: resolve(__dirname, 'src/auth/login.html'), // <--- Add this line for login.html
-        main_content_container: resolve(__dirname, 'src/components/main-content-container.js'),
-        sidebar_menu: resolve(__dirname, 'src/components/sidebar-menu.js'),
-        events_container: resolve(__dirname, 'src/events/events-container.js'),
-        friends_container: resolve(__dirname, 'src/friends/friends-container.js'),
-        profile_container: resolve(__dirname, 'src/profile/profile-container.js'),
-        settings_container: resolve(__dirname, 'src/settings/settings-container.js'),
+        // main_content_container: resolve(__dirname, 'src/components/main-content-container.js'),
+        // sidebar_menu: resolve(__dirname, 'src/components/sidebar-menu.js'),
+        // events_container: resolve(__dirname, 'src/events/events-container.js'),
+        // friends_container: resolve(__dirname, 'src/friends/friends-container.js'),
+        // profile_container: resolve(__dirname, 'src/profile/profile-container.js'),
+        // settings_container: resolve(__dirname, 'src/settings/settings-container.js'),
+
         // Add any other top-level HTML files here
       },
     },
