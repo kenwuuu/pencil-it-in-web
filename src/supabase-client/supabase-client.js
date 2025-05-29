af0192021de010318a5d5f98b08174dfb9247d41
kenwu <kenqiwu@gmail.com>
2025-05-29 00:03:38 -0700

    fix: add hardcoded strings for testing, apply auto formatting

--- src/supabase-client/supabase-client.js
+++ src/supabase-client/supabase-client.js
@@ -3,39 +3,35 @@
  * @description Initialize the Supabase client.
  */
 
-import {createClient} from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js';
 
 // Annoying code to make sure that we get the right variable whether we run this file through
 // Node or Vite. They don't play nice with each other.
 const isNode = typeof process !== 'undefined' && typeof process.env !== 'undefined';
 
-const supabaseUrl = isNode
-  ? process.env.VITE_SUPABASE_URL
-  : import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = isNode
-  ? process.env.VITE_SUPABASE_ANON_KEY
-  : import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://mpounklnfrcfpkefidfn.supabase.co/';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1wb3Vua2xuZnJjZnBrZWZpZGZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIxODE0OTcsImV4cCI6MjA1Nzc1NzQ5N30.wZlH6_dd0WtEVC-BtMXEzcTUgSAIlegqSPnr3dyvjyA';
 
 export const supabase = createClient(
-    supabaseUrl,
-    supabaseAnonKey);
+  supabaseUrl,
+  supabaseAnonKey);
 export const supabaseWithAuth = null;
 
 function getAccessToken() {
-    const accessToken = "todo";
-    return accessToken;
+  const accessToken = 'todo';
+  return accessToken;
 }
 
 export function getSupabaseClientWithAuth() {
-    const accessToken = getAccessToken();
+  const accessToken = getAccessToken();
 
-    return createClient(
-        supabaseUrl,
-        supabaseAnonKey, {
-        global: {
-            headers: {
-                Authorization: `Bearer ${accessToken}`,
-            },
-        },
+  return createClient(
+    supabaseUrl,
+    supabaseAnonKey, {
+      global: {
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      }
     });
 }
