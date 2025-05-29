c93c861524ebf151474819e9c276f6fd964d01e3
kenwu <kenqiwu@gmail.com>
2025-05-29 00:22:48 -0700

    fix: add API endpoint and public (anon) key as constants
    - these files aren't run on a server, only served, so there is no .env to rely on

--- src/supabase-client/supabase-client.js
+++ src/supabase-client/supabase-client.js
@@ -1,37 +1,12 @@
 /**
- * @file get-upcoming-events.js
- * @description Initialize the Supabase client.
+ * @file supabase-client.js
+ * @description Initialize a global Supabase client for our project to use.
  */
 
 import { createClient } from '@supabase/supabase-js';
-
-// Annoying code to make sure that we get the right variable whether we run this file through
-// Node or Vite. They don't play nice with each other.
-const isNode = typeof process !== 'undefined' && typeof process.env !== 'undefined';
-
-const supabaseUrl = 'https://mpounklnfrcfpkefidfn.supabase.co/';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1wb3Vua2xuZnJjZnBrZWZpZGZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIxODE0OTcsImV4cCI6MjA1Nzc1NzQ5N30.wZlH6_dd0WtEVC-BtMXEzcTUgSAIlegqSPnr3dyvjyA';
+import { SUPABASE_ANON_KEY, SUPABASE_URL } from '../../constants.js';
 
 export const supabase = createClient(
-  supabaseUrl,
-  supabaseAnonKey);
-export const supabaseWithAuth = null;
-
-function getAccessToken() {
-  const accessToken = 'todo';
-  return accessToken;
-}
-
-export function getSupabaseClientWithAuth() {
-  const accessToken = getAccessToken();
-
-  return createClient(
-    supabaseUrl,
-    supabaseAnonKey, {
-      global: {
-        headers: {
-          Authorization: `Bearer ${accessToken}`
-        }
-      }
-    });
-}
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
