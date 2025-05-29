5ed05b773856c5ad670049bdf272d931519e3d9d
kenwu <kenqiwu@gmail.com>
2025-05-29 00:04:06 -0700

    chore: add more js files

--- vite.config.ts
+++ vite.config.ts
@@ -11,17 +11,23 @@ export default defineConfig({
         events: resolve(__dirname, 'events.html'),
         index: resolve(__dirname, 'index.html'),
         login: resolve(__dirname, 'src/auth/login.html'),
+        // supabase
+        supabase: resolve(__dirname, 'src/supabase-client/supabase-client.js'),
         // components folder
         main_content_container: resolve(__dirname, 'src/components/main-content-container.js'),
         bottom_menu: resolve(__dirname, 'src/components/bottom-menu.js'),
         sidebar_menu: resolve(__dirname, 'src/components/sidebar-menu.js'),
         site_header: resolve(__dirname, 'src/components/site-header.js'),
-        //events
+        // events
         events_container: resolve(__dirname, 'src/events/events-container.js'),
         event_creation_component: resolve(__dirname, 'src/events/event-creation-component.js'),
         event_action_menu: resolve(__dirname, 'src/events/events-action-menu.js'),
+        get_upcoming_events: resolve(__dirname, 'src/events/services/get-upcoming-events.js'),
+        // profile
         profile: resolve(__dirname, 'src/profile/profile-container.js'),
+        // settings
         settings: resolve(__dirname, 'src/settings/settings-container.js'),
+        // friends
         friends: resolve(__dirname, 'src/friends/friends-container.js'),
       },
       // Ensure all JS modules are properly bundled
