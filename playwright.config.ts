7fa626cff3d6ff17d97119b29e9b329c256f8ccf
kenwu <kenqiwu@gmail.com>
2025-05-30 01:41:16 -0700

    fix: run setup as test. fix event tests. add env vars, remove manual auth generation

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,7 +37,10 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
-    { name: "setup", testMatch: /auth\.setup\.ts/ },
+    {
+      name: "setup",
+      testMatch: /auth\.setup\.ts/
+    },
 
     {
       name: "chromium",
