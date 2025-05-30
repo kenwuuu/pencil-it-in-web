4629ff79504585566bb75695c6e5e498d012c9b5
kenwu <kenqiwu@gmail.com>
2025-05-30 01:44:55 -0700

    fix: add environment variables, remove manual call to auth cookie generation

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -4,8 +4,8 @@ const isCI = process.env.CI?.toUpperCase() === "TRUE" || process.env.CI === "1";
 
 console.log("Running in CI mode: ", isCI);
 
-const EMAIL = isCI ? process.env.PLAYWRIGHT_KEN_EMAIL : "kenqiwu@gmail.com";
-const PASSWORD = isCI ? process.env.PLAYWRIGHT_KEN_PASSWORD : "adsihn9";
+const EMAIL = isCI ? process.env.PLAYWRIGHT_KEN_EMAIL : process.env.PENCIL_IT_IN_EMAIL;
+const PASSWORD = isCI ? process.env.PLAYWRIGHT_KEN_PASSWORD : process.env.PENCIL_IT_IN_PASSWORD;
 
 if (!EMAIL || !PASSWORD) {
   throw new Error("Missing login credentials: EMAIL or PASSWORD env vars are not set.");
