e9d9f1a0d73112915a7841bde9cdb7eb9cc4e889
kenwu <kenqiwu@gmail.com>
2025-05-30 23:53:27 -0700

    chore: apply auto formatting

--- src/auth/services/login.js
+++ src/auth/services/login.js
@@ -1,4 +1,4 @@
-import {supabase} from '../../supabase-client/supabase-client.js';
+import { supabase } from '../../supabase-client/supabase-client.js';
 
 /**
  * Logs in with email & password.
@@ -7,12 +7,12 @@ import {supabase} from '../../supabase-client/supabase-client.js';
  * @returns {Promise<{ user: object | null, session: object | null, error: object | null }>}
  */
 export async function loginWithEmailPassword(email, password) {
-    const {
-        data: {user, session},
-        error,
-    } = await supabase.auth.signInWithPassword({
-        email,
-        password,
-    });
-    return {user, session, error};
+  const {
+    data: { user, session },
+    error
+  } = await supabase.auth.signInWithPassword({
+    email,
+    password
+  });
+  return { user, session, error };
 }
