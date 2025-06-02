39ef0385e2dcc3e02c568c93f6ca61a40e5bfe5c
kenwu <kenqiwu@gmail.com>
2025-06-02 02:04:02 -0700

    chore: remove friend-search-component

--- src/friends/friends-container.js
+++ src/friends/friends-container.js
@@ -1,11 +1,10 @@
 import './friends-action-menu.js';
-import './friend-search-component.js';
 import './friends-search-bar.ts';
 
 class FriendsContainer extends HTMLElement {
-  connectedCallback() {
-    this.innerHTML = `
-      <main class="flex" x-data=" { is_searching_for_friends: false }">
+    connectedCallback() {
+        this.innerHTML = `
+      <main class="flex">
         <div
           class="page-container flex-1"
           hx-get="src/friends/mock_data/friends.html"
@@ -18,13 +17,12 @@ class FriendsContainer extends HTMLElement {
             </div>
             <friends-search-bar class="max-w-full"></friends-search-bar>
           </header>
-          <section class="friends-list" x-show="!is_searching_for_friends"></section>
-          <friend-search-component x-show="is_searching_for_friends"></friend-search-component>
+          <section class="friends-list"></section>
         </div>
         <friends-action-menu class="action-menu-side-component" src="src/mock_data/action_menu/friends.html" class="hidden xl:block"></friends-action-menu>
       </main>
     `;
-  }
+    }
 }
 
 customElements.define('friends-container', FriendsContainer);
