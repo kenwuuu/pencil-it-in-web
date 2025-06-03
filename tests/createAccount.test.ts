e5974a85fbd5378cc756aea6f32fb9107b3c4aa5
kenwu <kenqiwu@gmail.com>
2025-06-03 11:21:54 -0700

    fix: remove localhost from URL

--- tests/createAccount.test.ts
+++ tests/createAccount.test.ts
@@ -1,42 +1,42 @@
 import {expect, test} from "@playwright/test";
 
 test('testFirstNameFieldIsVisible', async ({page}) => {
-  await page.goto('http://localhost:5173/');
+  await page.goto('/');
   await page.getByRole('link', {name: 'Log In'}).click();
   await page.getByRole('link', {name: 'Create New Account'}).click();
   await expect(page.getByRole('textbox', {name: 'First name'})).toBeVisible();
 });
 
 test('testLastNameFieldIsVisible', async ({page}) => {
-  await page.goto('http://localhost:5173/');
+  await page.goto('/');
   await page.getByRole('link', {name: 'Log In'}).click();
   await page.getByRole('link', {name: 'Create New Account'}).click();
   await expect(page.getByRole('textbox', {name: 'Last name'})).toBeVisible();
 });
 
 test('testUsernameFieldIsVisible', async ({page}) => {
-  await page.goto('http://localhost:5173/');
+  await page.goto('/');
   await page.getByRole('link', {name: 'Log In'}).click();
   await page.getByRole('link', {name: 'Create New Account'}).click();
   await expect(page.getByRole('textbox', {name: 'Username'})).toBeVisible();
 });
 
 test('testEmailAddressFieldIsVisible', async ({page}) => {
-  await page.goto('http://localhost:5173/');
+  await page.goto('/');
   await page.getByRole('link', {name: 'Log In'}).click();
   await page.getByRole('link', {name: 'Create New Account'}).click();
   await expect(page.getByRole('textbox', {name: 'Email address'})).toBeVisible();
 });
 
 test('testPasswordFieldIsVisible', async ({page}) => {
-  await page.goto('http://localhost:5173/');
+  await page.goto('/');
   await page.getByRole('link', {name: 'Log In'}).click();
   await page.getByRole('link', {name: 'Create New Account'}).click();
   await expect(page.getByRole('textbox', {name: 'Password'})).toBeVisible();
 });
 
 test('testLogInButtonRedirectsToLoginPage', async ({page}) => {
-  await page.goto('http://localhost:5173/');
+  await page.goto('/');
   await page.getByRole('link', {name: 'Log In'}).click();
   await page.getByRole('link', {name: 'Create New Account'}).click();
   await page.getByRole('link', {name: 'Login'}).click();
