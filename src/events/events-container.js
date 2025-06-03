747bccf75214c9486b931fe420e58e1c704dfe6f
kenwu <kenqiwu@gmail.com>
2025-06-02 22:48:53 -0700

    chore: apply autoformatting

--- src/events/events-container.js
+++ src/events/events-container.js
@@ -1,15 +1,15 @@
 import './events-action-menu.js';
 import './event-creation-component.js';
 import './services/get-upcoming-events.js';
-import { supabase } from '../supabase-client/supabase-client.js';
-import { format, parseISO } from 'date-fns';
-import { getUpcomingEvents } from './services/get-upcoming-events.js';
-import { downloadICS } from './services/calendar.js';
+import {supabase} from '../supabase-client/supabase-client.js';
+import {format, parseISO} from 'date-fns';
+import {getUpcomingEvents} from './services/get-upcoming-events.js';
+import {downloadICS} from './services/calendar.js';
 
 class EventsContainer extends HTMLElement {
-  connectedCallback() {
-    this.innerHTML = `
-        <div class="flex" x-data=" { is_creating_new_event: false }">
+    connectedCallback() {
+        this.innerHTML = `
+        <div class="flex" x-data=" { is_creating_new_event: false }">  <!--   set this to true to always load into event creation component for testing   -->
           <div
             class="page-container flex-1"
             hx-trigger="load"
@@ -83,177 +83,177 @@ class EventsContainer extends HTMLElement {
           <events-action-menu class="action-menu-side-component"></events-action-menu>
         </div>
     `;
-  }
+    }
 }
 
 async function signIn() {
-  const { data: signInData, error: signInError } = await supabase.auth.signInWithPassword({
-    email: 'kenqiwu@gmail.com',
-    password: 'adsihn9'
-  });
+    const {data: signInData, error: signInError} = await supabase.auth.signInWithPassword({
+        email: 'kenqiwu@gmail.com',
+        password: 'adsihn9'
+    });
 
-  if (signInError) {
-    console.error('Sign-in error:', signInError);
-  }
+    if (signInError) {
+        console.error('Sign-in error:', signInError);
+    }
 }
 
 function selectEventElements() {
-  const template = document.querySelector('.event-card-template');
-  const agenda = document.querySelector('.events-agenda');
-  return { template, agenda };
+    const template = document.querySelector('.event-card-template');
+    const agenda = document.querySelector('.events-agenda');
+    return {template, agenda};
 }
 
 function populateEventElementsWithData(template, event, agenda) {
-  const templateClone = template.content.cloneNode(true);
+    const templateClone = template.content.cloneNode(true);
 
-  const date = parseISO(event['start_time']);
-  const startDate = format(date, 'MMMM do, yyyy'); // e.g., April 20th, 2025
-  const startTime = format(date, 'h:mmaaa').toLowerCase(); // e.g., 11:00pm
+    const date = parseISO(event['start_time']);
+    const startDate = format(date, 'MMMM do, yyyy'); // e.g., April 20th, 2025
+    const startTime = format(date, 'h:mmaaa').toLowerCase(); // e.g., 11:00pm
 
-  const titleElem = templateClone.querySelector('.title');
-  if (titleElem) {
-    titleElem.textContent = event.title;
-  }
+    const titleElem = templateClone.querySelector('.title');
+    if (titleElem) {
+        titleElem.textContent = event.title;
+    }
 
-  const hostElem = templateClone.querySelector('.host-user');
-  if (hostElem) {
-    hostElem.textContent = `Host: ${event.host[0].first_name}`;
-  }
+    const hostElem = templateClone.querySelector('.host-user');
+    if (hostElem) {
+        hostElem.textContent = `Host: ${event.host[0].first_name}`;
+    }
 
-  const descElem = templateClone.querySelector('.event-description');
-  if (descElem) {
-    descElem.textContent = event.description || '';
-  }
+    const descElem = templateClone.querySelector('.event-description');
+    if (descElem) {
+        descElem.textContent = event.description || '';
+    }
 
-  const startTimeElem = templateClone.querySelector('.event-time');
-  if (startTimeElem) {
-    startTimeElem.textContent = startTime;
-  }
+    const startTimeElem = templateClone.querySelector('.event-time');
+    if (startTimeElem) {
+        startTimeElem.textContent = startTime;
+    }
 
-  const startDateElem = templateClone.querySelector('.event-date');
-  if (startDateElem) {
-    startDateElem.textContent = startDate;
-  }
+    const startDateElem = templateClone.querySelector('.event-date');
+    if (startDateElem) {
+        startDateElem.textContent = startDate;
+    }
 
-  const yesButtonElem = templateClone.querySelector('.yes-button');
-  if (yesButtonElem) {
-    yesButtonElem.textContent = `Yes: ${event.attendance_yes_count}` || 'Yes: 0';
-  }
+    const yesButtonElem = templateClone.querySelector('.yes-button');
+    if (yesButtonElem) {
+        yesButtonElem.textContent = `Yes: ${event.attendance_yes_count}` || 'Yes: 0';
+    }
 
-  const maybeButtonElem = templateClone.querySelector('.maybe-button');
-  if (maybeButtonElem) {
-    maybeButtonElem.textContent = `Maybe: ${event.attendance_maybe_count}` || 'Maybe: 0';
-  }
+    const maybeButtonElem = templateClone.querySelector('.maybe-button');
+    if (maybeButtonElem) {
+        maybeButtonElem.textContent = `Maybe: ${event.attendance_maybe_count}` || 'Maybe: 0';
+    }
 
-  const noButtonElem = templateClone.querySelector('.no-button');
-  if (noButtonElem) {
-    noButtonElem.textContent = `No: ${event.attendance_no_count}` || 'No: 0';
-  }
+    const noButtonElem = templateClone.querySelector('.no-button');
+    if (noButtonElem) {
+        noButtonElem.textContent = `No: ${event.attendance_no_count}` || 'No: 0';
+    }
 
-  // querySelectorAll because we have two buttons that hide/show based on screen size
-  const downloadBtns = templateClone.querySelectorAll('.download-calendar-btn');
-  downloadBtns.forEach(btn => {
-    btn.addEventListener('click', () => downloadICS(event));
-  });
+    // querySelectorAll because we have two buttons that hide/show based on screen size
+    const downloadBtns = templateClone.querySelectorAll('.download-calendar-btn');
+    downloadBtns.forEach(btn => {
+        btn.addEventListener('click', () => downloadICS(event));
+    });
 
-  agenda.appendChild(templateClone);
+    agenda.appendChild(templateClone);
 }
 
 function mockGetUpcomingEvents() {
-  return [
-    {
-      'id': 'ff005eee-81d1-46f4-93bf-75966030fbf8',
-      'title': 'an obnoxiously long title because people suck and try to make my life hard',
-      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
-      'start_time': '2025-05-17T06:15:00+00:00',
-      'end_time': '2025-05-30T06:15:00+00:00',
-      'host': [
+    return [
         {
-          'user_id': '584850d8-59d1-4f48-b83d-85d97bd4dee6',
-          'first_name': 'Francesca'
-        }
-      ],
-      'created_at': '2025-04-26T13:15:36.138078+00:00',
-      'cohosts': [
-        null
-      ],
-      'participants': [
+            'id': 'ff005eee-81d1-46f4-93bf-75966030fbf8',
+            'title': 'an obnoxiously long title because people suck and try to make my life hard',
+            'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
+            'start_time': '2025-05-17T06:15:00+00:00',
+            'end_time': '2025-05-30T06:15:00+00:00',
+            'host': [
+                {
+                    'user_id': '584850d8-59d1-4f48-b83d-85d97bd4dee6',
+                    'first_name': 'Francesca'
+                }
+            ],
+            'created_at': '2025-04-26T13:15:36.138078+00:00',
+            'cohosts': [
+                null
+            ],
+            'participants': [
+                {
+                    'user_id': 'f18d6f00-b861-45bd-bad9-2d3c1b772323',
+                    'first_name': 'f1',
+                    'attendance_answer': 'yes'
+                }
+            ],
+            'attendance_yes_count': 12,
+            'attendance_maybe_count': 31,
+            'attendance_no_count': 9
+        },
         {
-          'user_id': 'f18d6f00-b861-45bd-bad9-2d3c1b772323',
-          'first_name': 'f1',
-          'attendance_answer': 'yes'
-        }
-      ],
-      'attendance_yes_count': 12,
-      'attendance_maybe_count': 31,
-      'attendance_no_count': 9
-    },
-    {
-      'id': 'c5fc677f-f021-40a7-81e4-da7ad8a46fa1',
-      'created_at': '2025-04-25T15:28:31.200507+00:00',
-      'title': 'picnic',
-      'description': null,
-      'start_time': '2025-06-22T19:10:25+00:00',
-      'end_time': '2025-06-22T22:10:25+00:00',
-      'host': [
+            'id': 'c5fc677f-f021-40a7-81e4-da7ad8a46fa1',
+            'created_at': '2025-04-25T15:28:31.200507+00:00',
+            'title': 'picnic',
+            'description': null,
+            'start_time': '2025-06-22T19:10:25+00:00',
+            'end_time': '2025-06-22T22:10:25+00:00',
+            'host': [
+                {
+                    'user_id': 'f18d6f00-b861-45bd-bad9-2d3c1b772323',
+                    'first_name': 'Francesca'
+                }
+            ],
+            'cohosts': [
+                null
+            ],
+            'participants': [
+                {
+                    'user_id': null,
+                    'first_name': null,
+                    'attendance_answer': null
+                }
+            ],
+            'attendance_yes_count': 0,
+            'attendance_maybe_count': 0,
+            'attendance_no_count': 0
+        },
         {
-          'user_id': 'f18d6f00-b861-45bd-bad9-2d3c1b772323',
-          'first_name': 'Francesca'
+            'id': 'd02afaa9-64ad-4abb-9151-7a35c0dadd19',
+            'title': 'anotherrr',
+            'description': null,
+            'start_time': '2025-07-10T09:45:00+00:00',
+            'end_time': '2025-07-26T09:45:00+00:00',
+            'host': [
+                {
+                    'user_id': '584850d8-59d1-4f48-b83d-85d97bd4dee6',
+                    'first_name': '58'
+                }
+            ],
+            'created_at': '2025-04-25T16:52:46.177964+00:00',
+            'cohosts': [
+                null
+            ],
+            'participants': [
+                {
+                    'user_id': 'f18d6f00-b861-45bd-bad9-2d3c1b772323',
+                    'first_name': 'f1',
+                    'attendance_answer': 'no'
+                }
+            ],
+            'attendance_yes_count': 0,
+            'attendance_maybe_count': 0,
+            'attendance_no_count': 1
         }
-      ],
-      'cohosts': [
-        null
-      ],
-      'participants': [
-        {
-          'user_id': null,
-          'first_name': null,
-          'attendance_answer': null
-        }
-      ],
-      'attendance_yes_count': 0,
-      'attendance_maybe_count': 0,
-      'attendance_no_count': 0
-    },
-    {
-      'id': 'd02afaa9-64ad-4abb-9151-7a35c0dadd19',
-      'title': 'anotherrr',
-      'description': null,
-      'start_time': '2025-07-10T09:45:00+00:00',
-      'end_time': '2025-07-26T09:45:00+00:00',
-      'host': [
-        {
-          'user_id': '584850d8-59d1-4f48-b83d-85d97bd4dee6',
-          'first_name': '58'
-        }
-      ],
-      'created_at': '2025-04-25T16:52:46.177964+00:00',
-      'cohosts': [
-        null
-      ],
-      'participants': [
-        {
-          'user_id': 'f18d6f00-b861-45bd-bad9-2d3c1b772323',
-          'first_name': 'f1',
-          'attendance_answer': 'no'
-        }
-      ],
-      'attendance_yes_count': 0,
-      'attendance_maybe_count': 0,
-      'attendance_no_count': 1
-    }
-  ];
+    ];
 }
 
 // on load: retrieves events from server and populates Agenda container with Event cards
 document.addEventListener('DOMContentLoaded', async () => {
-  let data = await getUpcomingEvents();
+    let data = await getUpcomingEvents();
 
-  const { template, agenda } = selectEventElements();
+    const {template, agenda} = selectEventElements();
 
-  data.forEach(event => {
-    populateEventElementsWithData(template, event, agenda);
-  });
+    data.forEach(event => {
+        populateEventElementsWithData(template, event, agenda);
+    });
 });
 
 
