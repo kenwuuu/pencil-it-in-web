9b491a23d30bd9d8b37fdb049e3b8b70eef8b491
kenwu <kenqiwu@gmail.com>
2025-06-05 15:47:08 -0700

    chore: shorten test timeout for on local
    bc i'm tired of long failures

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, devices } from "@playwright/test";
+import { defineConfig, devices } from '@playwright/test';
 
 /**
  * Read environment variables from file.
@@ -13,11 +13,13 @@ import { defineConfig, devices } from "@playwright/test";
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: "./tests",
+  testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
+  /* Set shorter timeout for local tests. Timeout applies to setup + test + teardown. Playwright default is 60s. */
+  timeout: process.env.CI ? 60_000 : 10_000,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Limit the number of failures on CI to save resources */
@@ -25,49 +27,49 @@ export default defineConfig({
   /* Opt out of parallel tests on CI for stability & reproducibility. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.BASE_URL || "http://localhost:5173",
+    baseURL: process.env.BASE_URL || 'http://localhost:5173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry"
+    trace: 'on-first-retry',
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: "setup",
-      testMatch: /auth\.setup\.ts/
+      name: 'setup',
+      testMatch: /auth\.setup\.ts/,
     },
 
     {
-      name: "chromium",
+      name: 'chromium',
       use: {
-        ...devices["Desktop Chrome"],
-        storageState: "tests/playwright.auth.json"
+        ...devices['Desktop Chrome'],
+        storageState: 'tests/playwright.auth.json',
       },
-      dependencies: ["setup"]
+      dependencies: ['setup'],
     },
 
     {
-      name: "firefox",
+      name: 'firefox',
       use: {
-        ...devices["Desktop Firefox"],
-        storageState: "tests/playwright.auth.json"
+        ...devices['Desktop Firefox'],
+        storageState: 'tests/playwright.auth.json',
       },
-      dependencies: ["setup"]
+      dependencies: ['setup'],
     },
 
     {
-      name: "webkit",
+      name: 'webkit',
       use: {
-        ...devices["Desktop Safari"],
-        storageState: "tests/playwright.auth.json"
+        ...devices['Desktop Safari'],
+        storageState: 'tests/playwright.auth.json',
       },
-      dependencies: ["setup"]
-    }
+      dependencies: ['setup'],
+    },
 
     /* Test against mobile viewports. */
     // {
@@ -88,7 +90,7 @@ export default defineConfig({
     //   name: 'Google Chrome',
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
-  ]
+  ],
 
   /* Run your local dev server before starting the tests */
   // webServer: {
