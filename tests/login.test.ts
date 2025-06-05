6696ea429a49a6b7f7b2b5f5b9a993a2aa9322de
kenwu <kenqiwu@gmail.com>
2025-06-05 15:44:34 -0700

    feat: have index.html redirect based off user session and user agent

--- tests/login.test.ts
+++ tests/login.test.ts
@@ -1,39 +1,36 @@
-import { expect, test } from "@playwright/test";
-import { getCredentials } from "./credentialUtil";
+import { expect, test } from '@playwright/test';
+import { getCredentials } from './credentialUtil';
 
 const { EMAIL, PASSWORD } = getCredentials();
 
-test("testLoginRedirectsToEventsPage", async ({ page }) => {
-  await page.goto("/");
-  await page.getByRole("link", { name: "Log In" }).click();
-  await page.getByRole("textbox", { name: "Email address" }).click();
-  await page
-    .getByRole("textbox", { name: "Email address" })
-    .fill(EMAIL);
-  await page.getByRole("textbox", { name: "Password" }).click();
-  await page.getByRole("textbox", { name: "Password" }).fill(PASSWORD);
-  await page.getByRole("button", { name: "Log in" }).click();
-  await expect(page.getByText("pencil it in")).toBeVisible();
+test('testLoginPageRedirectsToEventsPage', async ({ page }) => {
+  await page.goto('/src/auth/login.html');
+  await page.getByRole('textbox', { name: 'Email address' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).fill(EMAIL);
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill(PASSWORD);
+  await page.getByRole('button', { name: 'Log in' }).click();
+  await expect(page.getByText('pencil it in')).toBeVisible();
 });
 
-test("testCreateAccountButtonRedirectsToCreateAccountPage", async ({
-                                                                     page
-                                                                   }) => {
-  await page.goto("/src/auth/login.html#");
-  await page.getByRole("link", { name: "Create New Account" }).click();
-  await expect(page.getByRole("heading")).toContainText("Create an account");
+test('testCreateAccountButtonRedirectsToCreateAccountPage', async ({
+  page,
+}) => {
+  await page.goto('/src/auth/login.html#');
+  await page.getByRole('link', { name: 'Create New Account' }).click();
+  await expect(page.getByRole('heading')).toContainText('Create an account');
 });
 
-test("testForgotPasswordButtonRedirectsToPasswordResetPage", async ({
-                                                                      page
-                                                                    }) => {
-  await page.goto("/src/auth/login.html#");
-  await page.getByRole("link", { name: "Forgot password?" }).click();
+test('testForgotPasswordButtonRedirectsToPasswordResetPage', async ({
+  page,
+}) => {
+  await page.goto('/src/auth/login.html#');
+  await page.getByRole('link', { name: 'Forgot password?' }).click();
 
   // todo add password reset page and then remove this skip
-  await test.step.skip("not yet ready", async () => {
-    await expect(page.getByRole("heading")).toContainText(
-      "Reset your password"
+  await test.step.skip('not yet ready', async () => {
+    await expect(page.getByRole('heading')).toContainText(
+      'Reset your password',
     );
   });
 });
