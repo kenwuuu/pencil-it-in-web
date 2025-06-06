8447be0d7bdd6a45648899abe602064a8feaab13
kenwuuu <kenwuuu@users.noreply.github.com>
2025-06-06 14:18:00 -0700

    feat: connect profile page to database (#143)
    * feat: add premade profile card
    
    * chore: remove unused console.log

--- src/profile/services/get-profile.ts
+++ src/profile/services/get-profile.ts
@@ -1,9 +1,9 @@
 import { supabase } from '@/supabase-client/supabase-client';
 
-const edgeFunctionName = 'retrieve-user-friends';
+const edgeFunctionName = 'get-user';
 
 // Function to call the edge function
-export async function getUserFriends() {
+export async function getUser(user_id: string) {
   const { data, error } = await supabase.auth.getSession();
   let token = data?.session?.access_token; // Get the user's access token
   if (error) {
@@ -13,11 +13,12 @@ export async function getUserFriends() {
   const response = await fetch(
     `https://mpounklnfrcfpkefidfn.supabase.co/functions/v1/${edgeFunctionName}`,
     {
-      method: 'GET',
+      method: 'POST',
       headers: {
         Authorization: `Bearer ${token}`,
         'Content-Type': 'application/json',
       },
+      body: JSON.stringify({ user_id: user_id }),
     },
   );
 
