ed19b5a988d365a0b3005f30f6e15764d20738e2
kenwuuu <kenwuuu@users.noreply.github.com>
2025-06-08 02:56:34 -0700

    fix: site 404's on load (#158)
    fix:
    - add landing page to vite
    
    added:
    - tests
    - start-server-and-test
    - automatic formatting

--- tests/friends.test.ts
+++ tests/friends.test.ts
@@ -1,31 +1,37 @@
-import {expect, test} from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
-test('testFriendsContainerIsVisible', async ({page}) => {
+test('testFriendsContainerIsVisible', async ({ page }) => {
   await page.goto('/events.html');
   await page.getByTestId('friends-menu-item').click();
   await expect(page.getByText('Friends within 30 miles of you')).toBeVisible();
 });
 
-test('testFriendsListIsVisible', async ({page}) => {
+test('testFriendsListIsVisible', async ({ page }) => {
   await page.goto('/events.html');
   await page.getByTestId('friends-menu-item').click();
-  await expect(page.getByRole('heading', {name: 'Friends'})).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Friends' })).toBeVisible();
 });
 
-test('testFriendsSearchBarIsVisible', async ({page}) => {
+test('testFriendsSearchBarIsVisible', async ({ page }) => {
   await page.goto('/events.html');
   await page.getByTestId('friends-menu-item').click();
-  await expect(page.locator('header').filter({hasText: 'Friends Add Friend'}).locator('div').nth(1)).toBeVisible();
+  await expect(
+    page
+      .locator('header')
+      .filter({ hasText: 'Friends Add Friend' })
+      .locator('div')
+      .nth(1),
+  ).toBeVisible();
 });
 
-test('testFriendProfilePictureIsVisible', async ({page}) => {
+test('testFriendProfilePictureIsVisible', async ({ page }) => {
   await page.goto('/events.html');
   await page.getByTestId('friends-menu-item').click();
-  await expect(page.locator('.size-10').first()).toBeVisible();
+  await expect(page.locator('.list-row > div').first()).toBeVisible();
 });
 
-test('testFriendsSearchButtonIsVisible', async ({page}) => {
+test('testFriendsSearchButtonIsVisible', async ({ page }) => {
   await page.goto('/events.html');
   await page.getByTestId('friends-menu-item').click();
-  await expect(page.getByRole('button', {name: 'Add Friend'})).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Add Friend' })).toBeVisible();
 });
