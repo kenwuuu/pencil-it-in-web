c80e471ea810575cf2a14b0504d33ea6f0a0a3c2
kenwu <kenqiwu@gmail.com>
2025-06-09 16:47:49 -0700

    feat: users can now upload a profile photo and add username when creating an account

--- src/auth/services/signup.js
+++ src/auth/services/signup.js
@@ -2,6 +2,10 @@ import { supabase } from '../../supabase-client/supabase-client.js';
 
 /**
  * Sign up a new user with email & password.
+ * @param firstName
+ * @param lastName
+ * @param username
+ * @param profilePhoto
  * @param {string} email
  * @param {string} password
  * @returns {Promise<{ user: object | null, session: object | null, error: object | null }>}
@@ -10,9 +14,9 @@ export async function signUpUser(
   firstName,
   lastName,
   username,
-  profilePhoto,
   email,
   password,
+  profilePhoto,
 ) {
   const {
     data: { user, session },
@@ -22,34 +26,73 @@ export async function signUpUser(
     password,
   });
 
+  if (error) {
+    console.error(error);
+    return;
+  }
+
   const userId = user.id;
+
+  console.log(firstName);
+  console.log(lastName);
+
+  // profilePhoto = compressPhoto();
+  const filePath = await uploadFile(profilePhoto);
+  const photoUrl = buildBucketUrl(filePath);
+
+  insertPhotoAndNamesToUserRow(userId, firstName, lastName, username, photoUrl);
 
   // compress photo to AVIF
   function compressPhoto(file) {
-    return photo;
+    return file;
   }
 
   // upload profile photo
   async function uploadFile(file) {
+    const uuid = crypto.randomUUID(); // generates a UUID v4
+
     const { data, error } = await supabase.storage
-      .from('profile_photos')
-      // replace file_path with gen4 uuid
-      .upload('file_path', file, {
-        contentType: 'image/avif',
+      .from('profile-photos')
+      .upload(uuid, file, {
+        contentType: file.type || 'image/jpg',
       });
+
     if (error) {
-      // Handle error
+      console.error('Upload failed:', error);
     } else {
-      // Handle success
+      console.log('Upload successful:', data);
+      return data.path;
     }
   }
 
-  async function insertPhotoUrlToUserRow(userId, photoFilePath) {
-    let { data, error } = await supabase
-      .from('users')
-      .update({ profile_photo_url: photoFilePath })
-      .eq('id', userId);
+  function buildBucketUrl(filePath) {
+    const bucketUrl =
+      'https://mpounklnfrcfpkefidfn.supabase.co/storage/v1/object/public/profile-photos/';
+    return bucketUrl + filePath;
   }
 
-  return { user, session, error };
+  async function insertPhotoAndNamesToUserRow(
+    userId,
+    firstName,
+    lastName,
+    username,
+    photoFilePath,
+  ) {
+    let { data, error } = await supabase
+      .from('users')
+      .update({
+        profile_photo_url: photoFilePath,
+        first_name: firstName,
+        last_name: lastName,
+        username: username,
+      })
+      .eq('id', userId);
+    if (data) {
+      console.log(data);
+    } else if (error) {
+      console.log(error);
+    }
+  }
+
+  // return { user, session, error };
 }
