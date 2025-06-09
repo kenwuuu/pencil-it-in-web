324b721bd2a98bc878f4b6b0b73c652503d95d34
kenwu <kenqiwu@gmail.com>
2025-06-09 13:40:46 -0700

    feat: add signup profile photo boilerplate

--- src/auth/services/signup.js
+++ src/auth/services/signup.js
@@ -1,4 +1,4 @@
-import {supabase} from '../../supabase-client/supabase-client.js';
+import { supabase } from '../../supabase-client/supabase-client.js';
 
 /**
  * Sign up a new user with email & password.
@@ -6,13 +6,50 @@ import {supabase} from '../../supabase-client/supabase-client.js';
  * @param {string} password
  * @returns {Promise<{ user: object | null, session: object | null, error: object | null }>}
  */
-export async function signUpUser(email, password) {
-    const {
-        data: {user, session},
-        error,
-    } = await supabase.auth.signUp({
-        email,
-        password,
-    });
-    return {user, session, error};
+export async function signUpUser(
+  firstName,
+  lastName,
+  username,
+  profilePhoto,
+  email,
+  password,
+) {
+  const {
+    data: { user, session },
+    error,
+  } = await supabase.auth.signUp({
+    email,
+    password,
+  });
+
+  const userId = user.id;
+
+  // compress photo to AVIF
+  function compressPhoto(file) {
+    return photo;
+  }
+
+  // upload profile photo
+  async function uploadFile(file) {
+    const { data, error } = await supabase.storage
+      .from('profile_photos')
+      // replace file_path with gen4 uuid
+      .upload('file_path', file, {
+        contentType: 'image/avif',
+      });
+    if (error) {
+      // Handle error
+    } else {
+      // Handle success
+    }
+  }
+
+  async function insertPhotoUrlToUserRow(userId, photoFilePath) {
+    let { data, error } = await supabase
+      .from('users')
+      .update({ profile_photo_url: photoFilePath })
+      .eq('id', userId);
+  }
+
+  return { user, session, error };
 }
