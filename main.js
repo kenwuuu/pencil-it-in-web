8a19362e05c40ada007f111793aa5b9103bde942
kenwu <kenqiwu@gmail.com>
2025-06-11 23:09:23 -0700

    move auto theme detection to main.js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
+
+// initialize Alpine's global state management. required to happen on init.
 Alpine.store('profile_photo', {
   url: '',
   set(newUrl) {
@@ -9,5 +11,24 @@ Alpine.store('profile_photo', {
 });
 
 Alpine.start();
+// end mandatory alpine stuff
 
+// hacky way to make DaisyUI button animations work on iOS
 document.addEventListener('touchstart', () => {}, true);
+
+// dark mode script
+const setTheme = () => {
+  const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  document.documentElement.setAttribute(
+    'data-theme',
+    prefersDark ? 'cupcake_dark' : 'cupcake',
+  );
+};
+
+// auto switch when system switches
+window
+  .matchMedia('(prefers-color-scheme: dark)')
+  .addEventListener('change', setTheme);
+
+// Initial load
+setTheme();
