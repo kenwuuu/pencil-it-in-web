bc5edf5d4cc322fabc87c93ad5d34fad76f37a68
kenwu <kenqiwu@gmail.com>
2025-06-14 10:14:31 -0700

    Enlarge dock icons, replace labels with ARIA labels

--- src/components/bottom-menu.js
+++ src/components/bottom-menu.js
@@ -5,28 +5,28 @@ class BottomMenu extends HTMLElement {
     this.innerHTML = `
         <div class="dock">
           <button
+            aria-label="events"
             x-on:click="page = 'events'"
             id="bottom-menu-item-events" 
             x-bind:class="page === 'events' ? 'dock-active' : ''"
             >
-          <iconify-icon icon="mdi:calendar"></iconify-icon>
-          Events
+          <iconify-icon class="text-2xl" icon="mdi:calendar"></iconify-icon>
           </button>
           <button
+            aria-label="friends"
             x-on:click="page = 'friends'"
             id="bottom-menu-item-friends" 
             x-bind:class="page === 'friends' ? 'dock-active' : ''"
             >
-          <iconify-icon icon="mdi:account-group"></iconify-icon>
-          Friends
+          <iconify-icon class="text-2xl" icon="mdi:account-group"></iconify-icon>
           </button>
           <button
+            aria-label="profile"
             x-on:click="page = 'profile'"
             id="bottom-menu-item-profile" 
             x-bind:class="page === 'profile' ? 'dock-active' : ''"
             >
-          <iconify-icon icon="mdi:user"></iconify-icon>
-          Profile
+          <iconify-icon class="text-2xl" icon="mdi:user"></iconify-icon>
           </button>
         </div>
     `;
