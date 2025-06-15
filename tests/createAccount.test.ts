9bec17eb9b57ca433482cc84e256c564a4bb713f
kenwu <kenqiwu@gmail.com>
2025-06-14 17:44:42 -0700

    Add template for a create account test
    Signed-off-by: kenwu <kenqiwu@gmail.com>

--- tests/createAccount.test.ts
+++ tests/createAccount.test.ts
@@ -8,3 +8,26 @@ test('testCreateAccountPageRedirectsToLoginPage', async ({ page }) => {
     page.getByRole('heading', { name: 'Log in to your account' }),
   ).toBeVisible();
 });
+
+test.use({ storageState: { cookies: [], origins: [] } }); // run without cookies
+test('testCreateAccount', async ({ page }) => {
+  await page.goto('/src/auth/create-account.html');
+  await page.getByRole('textbox', { name: 'First name' }).click();
+  await page.getByRole('textbox', { name: 'First name' }).fill('ken');
+  await page.getByRole('textbox', { name: 'First name' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Last name' }).fill('ken');
+  await page.getByRole('textbox', { name: 'Last name' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Username' }).fill('kenken');
+  await page.getByRole('textbox', { name: 'Username' }).press('Tab');
+  await page
+    .getByRole('button', { name: 'Create Account' })
+    .scrollIntoViewIfNeeded();
+  await page
+    .getByRole('textbox', { name: 'Email address' })
+    .fill('matria972@gmail.com');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('Atwf3@vaw');
+  await page.locator('input[type="file"]').setInputFiles('IMG_6436.jpeg');
+
+  await expect(page.getByText('Please check your email for a')).toBeVisible();
+});
