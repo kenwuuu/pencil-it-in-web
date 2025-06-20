7c9df5dd740d58c8623494d7e55f37d02a7487d7
kenwuuu <kenwuuu@users.noreply.github.com>
2025-06-20 12:22:22 -0700

    Write test for create account (#291)
    * Add template for a create account test
    * Add email and password to .env
    
    Signed-off-by: kenwu <kenqiwu@gmail.com>

--- tests/createAccount.test.ts
+++ tests/createAccount.test.ts
@@ -1,4 +1,26 @@
 import { expect, test } from '@playwright/test';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const TESTING_EMAIL = process.env.TESTING_EMAIL || '';
+const TESTING_PASSWORD = process.env.TESTING_PASSWORD || '';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+if (TESTING_EMAIL === '') {
+  throw new Error(
+    'TESTING_EMAIL environment variable is not set. Please add it to your .env file in project root or to your environment configuration.',
+  );
+}
+
+if (TESTING_PASSWORD === '') {
+  throw new Error(
+    'TESTING_PASSWORD environment variable is not set. Please add it to your .env file in project root or to your environment configuration.',
+  );
+}
 
 test.use({ storageState: { cookies: [], origins: [] } }); // run without cookies
 test('testCreateAccountPageRedirectsToLoginPage', async ({ page }) => {
@@ -7,4 +29,43 @@ test('testCreateAccountPageRedirectsToLoginPage', async ({ page }) => {
   await expect(
     page.getByRole('heading', { name: 'Log in to your account' }),
   ).toBeVisible();
+});
+
+// todo this currently passes even if the user already exists. we need to check for duplicate accounts on the frontend
+// and prevent signups
+test.use({ storageState: { cookies: [], origins: [] } }); // run without cookies
+test('testCreateAccount', async ({ page }) => {
+  await page.goto('/src/auth/create-account.html');
+  await page.getByRole('textbox', { name: 'First name' }).click();
+  await page.getByRole('textbox', { name: 'First name' }).fill('e2eTest');
+  await page.getByRole('textbox', { name: 'First name' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Last name' }).fill('e2eTest');
+  await page.getByRole('textbox', { name: 'Last name' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Username' }).fill('e2e_test');
+  await page.getByRole('textbox', { name: 'Username' }).press('Tab');
+  await page
+    .getByRole('button', { name: 'Create Account' })
+    .scrollIntoViewIfNeeded();
+  await page
+    .getByRole('textbox', { name: 'Email address' })
+    .fill(TESTING_EMAIL);
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('Atwf3@vaw');
+  const imagePath = path.resolve(__dirname, 'IMG_6436.jpeg');
+  await page.locator('input[type="file"]').setInputFiles(imagePath);
+  await page.getByRole('button', { name: 'Create Account' }).click();
+
+  await expect(page.getByText('Please check your email for')).toBeVisible();
+});
+
+test.use({ storageState: { cookies: [], origins: [] } }); // run without cookies
+test('testPhotoUploadWorks', async ({ page }) => {
+  await page.goto('/src/auth/create-account.html');
+  await page
+    .getByRole('button', { name: 'Create Account' })
+    .scrollIntoViewIfNeeded();
+  const imagePath = path.resolve(__dirname, 'IMG_6436.jpeg');
+  await page.locator('input[type="file"]').setInputFiles(imagePath);
+
+  await expect(page.getByText('IMG_6436.jpeg')).toBeVisible();
 });
