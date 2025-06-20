4c39b251d06d1203cb44c2c5a4e2c0433454fe81
kenwuuu <kenwuuu@users.noreply.github.com>
2025-06-20 11:33:31 -0700

    Add constants file (#290)
    * Rename constants.ts to secrets.ts
    * Add constants.ts file
    * Move Supabase URL and public key to their own file
    
    Signed-off-by: kenwu <kenqiwu@gmail.com>

---
+++ supabase-public.ts
@@ -0,0 +1,3 @@
+export const SUPABASE_URL = 'https://mpounklnfrcfpkefidfn.supabase.co/';
+export const SUPABASE_ANON_KEY =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1wb3Vua2xuZnJjZnBrZWZpZGZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIxODE0OTcsImV4cCI6MjA1Nzc1NzQ5N30.wZlH6_dd0WtEVC-BtMXEzcTUgSAIlegqSPnr3dyvjyA';
