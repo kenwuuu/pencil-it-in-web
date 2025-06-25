0c020269d1834683a63170f2a0972cf7e864889c
kenwuuu <kenwuuu@users.noreply.github.com>
2025-06-25 12:26:20 -0700

    Redirect to login if user is not logged in (#338)
    * Redirect to login if user is not logged in
    
    Signed-off-by: kenwu <kenqiwu@gmail.com>
    
    * Fix cookies being cleared globally
    
    Signed-off-by: kenwu <kenqiwu@gmail.com>
    
    ---------
    
    Signed-off-by: kenwu <kenqiwu@gmail.com>

---
+++ src/auth/services/redirect.js
@@ -0,0 +1,28 @@
+import { supabase } from '@/supabase-client/supabase-client.js';
+
+// For the root page (index.html), redirect users based on their auth status.
+export function handleRootRedirect() {
+  supabase.auth.getSession().then(({ data }) => {
+    const token = data?.session?.access_token;
+    const isMobile = /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
+
+    if (token) {
+      window.location.href = '/events.html';
+    } else if (isMobile) {
+      window.location.href = '/src/auth/create-account.html';
+    } else {
+      window.location.href = '/src/auth/login.html';
+    }
+  });
+}
+
+// For protected pages, redirect users to login if they are not authenticated.
+export function protectPage() {
+  supabase.auth.getSession().then(({ data }) => {
+    const token = !data?.session?.access_token;
+    console.log('token', token);
+    if (token) {
+      window.location.href = '/src/auth/login.html';
+    }
+  });
+}
--- tests/events.test.ts
+++ tests/events.test.ts
@@ -117,3 +117,14 @@ test('testDeleteEventButtonIsVisible', async ({ page }) => {
 });
 
 // add tests that confirm delete button only appears for host after we set up mocks
+
+test.describe('tests without auth cookies', () => {
+  test.use({ storageState: { cookies: [], origins: [] } });
+
+  test('testRedirectToLoginWhenNotLoggedIn', async ({ page }) => {
+    await page.goto('/events.html');
+    await expect(
+      page.getByRole('heading', { name: 'Log in to your account' }),
+    ).toBeVisible();
+  });
+});
