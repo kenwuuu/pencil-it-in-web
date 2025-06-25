48c5eb503e228c208bf701cb92463acba12bebed
kenwu <kenqiwu@gmail.com>
2025-06-25 15:51:41 -0700

    Add VAPID service worker code, install web-push as dev dependency
    Signed-off-by: kenwu <kenqiwu@gmail.com>

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
 import 'src/startup-scripts/alpine-script.ts'; // Alpine code
 import 'src/startup-scripts/theming-script.ts'; // THEME AND APPEARANCE CODE
 import 'src/startup-scripts/notifications-script.ts'; // START CAPACITOR NOTIFICATION SETUP
+import 'src/startup-scripts/web-push-notifications.js'; // START PWA NOTIFICATION SETUP
